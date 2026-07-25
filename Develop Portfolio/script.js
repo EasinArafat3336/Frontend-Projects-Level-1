b02b95286af8a134c65fe073397cb6ea9271dd97
Easin Arafat <arafateassin1@gmail.com>
2026-07-25 23:17:35 +0600

    Added sidebar with animation and modified responsive navbar form tailwind to css

---
+++ Develop Portfolio/script.js
@@ -0,0 +1,16 @@
+let menuIcon = document.querySelector(".menuIcon");
+let sidebar = document.querySelector(".sidebar");
+
+menuIcon.addEventListener("click",()=>{
+    if(sidebar.classList.contains("controlSidebar")){
+        sidebar.classList.remove("controlSidebar")
+        sidebar.classList.add("sidebarOpen")
+    }else if(sidebar.classList.contains("sidebarOpen")){
+        sidebar.classList.remove("sidebarOpen")
+        sidebar.classList.add("sidebarClose")
+    }
+    else{
+        sidebar.classList.remove("sidebarClose")
+        sidebar.classList.add("sidebarOpen")
+    }
+})
